9f2d9dd0a91cd5d6e87e8ac96f0f3b7a9b24c53d
GoldUkulele <kcramos1211@gmail.com>
2019-03-13 23:42:58 +0000

    Linking paths

--- app/controllers/dancers_controller.rb
+++ app/controllers/dancers_controller.rb
@@ -2,4 +2,19 @@ class DancersController < ApplicationController
   def index
     @dancers = Dancer.all
   end
+
+  def new
+    @dancer = Dancer.new
+  end
+
+  def create
+    Dancer.create(dancer_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def dancer_params
+    params.require(:dancer).permit(:name, :bio, :style)
+  end
 end
